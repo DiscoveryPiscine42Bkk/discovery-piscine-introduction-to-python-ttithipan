1e2dc4ed34d09e8378b2827b41885e0c906c7ef7
Tithipan <107382768+Tithipan@users.noreply.github.com>
2025-06-24 23:16:31 +0700

    add 2.3

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,14 @@
+number1 = input("Enter the first number: ")
+number2 = input("Enter the second number: ")
+try:
+    number1, number2 = int(number1), int(number2)
+    result = number1 * number2
+    print(number1, "X", number2, "=", result)
+    if result == 0:
+        print("The result is positive and negative.")
+    elif result < 0:
+        print("The result is negative.")
+    else:
+        print("The result is positive.")
+except:
+    raise ValueError
