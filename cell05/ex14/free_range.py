34adc3c18b7f5e7c888974795d117578eaae3dab
Tithipan <107382768+Tithipan@users.noreply.github.com>
2025-06-26 12:01:26 +0700

    Add fix

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -1,5 +1,8 @@
 import sys
 new_list = []
-for number in range(int(sys.argv[1]), int(sys.argv[2])+1):
-    new_list.append(number)
-print(new_list)
+if len(sys.argv) <3 :
+    print("None")
+else:    
+    for number in range(int(sys.argv[1]), int(sys.argv[2])+1):
+        new_list.append(number)
+    print(new_list)
