5fc0f91cf9a81950ed7edb86066ce49142fe6dbd
Tithipan <107382768+Tithipan@users.noreply.github.com>
2025-06-26 21:21:57 +0700

    change code

--- rush/checkmate.py
+++ rush/checkmate.py
@@ -1,107 +1,171 @@
-def check_board_size(board_as_string):
-  for row in board_as_string.split():
-    if len(row) != len(board_as_string.split()):
-      return "Invalid board size"
-  return len(board_as_string.split())
-#If valid board size, return size, else return invalid board size.
+pawn_direction = [(-1, -1), (-1, 1)]
+rook_direction = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+bishop_direction = [(-1, -1), (-1, 1), (1, 1), (1, -1)] #(from 1,1) to 0 0 | 0 2 | 2 2 | 2 0
+queen_direction = rook_direction + bishop_direction
+king_direction = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
+king_in_check = False
 
-def game_state_iterator(board, size):
-  try:
-    size = int(size)
-    record = []
-    for rows in range(0, size):
-      for columns in range(0, size):
-        if board[rows][columns] == "P":
-          record.append(pawn_attack_pattern(board, rows, columns, size))
-        elif board[rows][columns] in ["R", "Q"]:
-          record.append(rook_attack_pattern(board, rows, columns, size))
-        elif board[rows][columns] in ["B", "Q"]:
-          record.append(bishop_attack_pattern(board, rows, columns, size))
-    if True in record:
-      return "Success"
+
+def print_board(board):
+    for line in board:
+        print(line)
+
+def board_checker(board):
+    row_count = 0
+    board_count = 0
+    for row in range(len(board)):
+        row_count += 1
+        for space in range(len(board[row])):
+            board_count += 1
+    # print(board_count, row_count) # for checking board size
+    if board_count != row_count ** 2:
+        print("Invalid Board: Not A Square Board")
+        exit()
     else:
-      return "Fail"
-  except ValueError:
-    return "Invalid board size"
+        print("Valid Board")
 
-def pawn_attack_pattern(board, rows, columns, size):
-  if rows == 0:
-    return False
-  if columns > 0:
-    if board[rows - 1][columns - 1] == "K":
-      return True
-  if columns < size - 1:  #minus 1 because we start from 0 to 7, meaning board size 8.
-    if board[rows - 1][columns + 1] == "K":
-      return True
-  return False
-#Pawn attack pattern is done.
-def rook_attack_pattern(board, rows, columns, size):
-  obstacle = False
-  for checker in range(1, size):
-    if columns + checker < size:
-      if board[rows][columns + checker] in ["Q", "R", "B", "P"]:
-        obstacle = True
-      else:
-        if board[rows][columns + checker] == "K" and not obstacle:
-          return True
-  obstacle = False
-  for checker in range(1, size):
-    if columns - checker >= 0:
-      if board[rows][columns - checker] in ["Q", "R", "B", "P"]:
-        obstacle = True
-      else:
-        if board[rows][columns - checker] == "K" and not obstacle:
-          return True
-  obstacle = False
-  for checker in range(1, size):
-    if rows + checker < size:
-      if board[rows + checker][columns] in ["Q", "R", "B", "P"]:
-        obstacle = True
-      else:
-        if board[rows + checker][columns] == "K" and not obstacle:
-          return True
-  obstacle = False
-  for checker in range(1, size):
-    if rows - checker >= 0:
-      if board[rows - checker][columns] in ["Q", "R", "B", "P"]:
-        obstacle = True
-      else:
-        if board[rows - checker][columns] == "K" and not obstacle:
-          return True
-  return False
-#Rook attack pattern is done.
-def bishop_attack_pattern(board, rows, columns, size):
-  obstacle = False
-  for checker in range(1, size):
-    if columns + checker < size and rows + checker < size:
-      if board[rows + checker][columns + checker] in ["Q", "R", "B", "P"]:
-        obstacle = True
-      else:
-        if board[rows + checker][columns + checker] == "K" and not obstacle:
-          return True
-  obstacle = False
-  for checker in range(1, size):
-    if columns - checker >= 0 and rows - checker >= 0:
-      if board[rows - checker][columns - checker] in ["Q", "R", "B", "P"]:
-        obstacle = True
-      else:
-        if board[rows - checker][columns - checker] == "K" and not obstacle:
-          return True
-  obstacle = False
-  for checker in range(1, size):
-    if columns + checker < size and rows - checker >= 0:
-      if board[rows - checker][columns + checker] in ["Q", "R", "B", "P"]:
-        obstacle = True
-      else:
-        if board[rows - checker][columns + checker] == "K" and not obstacle:
-          return True
-  obstacle = False
-  for checker in range(1, size):
-    if columns - checker >= 0 and rows + checker < size:
-      if board[rows + checker][columns - checker] in ["Q", "R", "B", "P"]:
-        obstacle = True
-      else:
-        if board[rows + checker][columns - checker] == "K" and not obstacle:
-          return True
-  return False
-#Bishop attack pattern is done.
+"""
+def pawn_move(ori_x, ori_y): # go through
+    for dx, dy in pawn_direction:
+        i, j = ori_x + dx, ori_y + dy
+        if board[i][j] == '.':
+            board[i][j] = 'C'
+        elif board[i][j] == 'K':
+            print("Success")
+            exit()
+        elif board[i][j] == 'C':
+            i += dx
+            j += dy
+        else:
+            break
+"""
+
+def pawn_move(board, ori_x, ori_y): # not go through
+    for dx, dy in pawn_direction:
+        i, j = ori_x + dx, ori_y + dy
+        if 0 <= i < len(board) and 0 <= j < len(board[i]):
+            if board[i][j] == 'K':
+                return True
+                # print("Success")
+                # exit()
+            elif board[i][j] == '.':
+                board[i][j] = 'C'
+
+# 4,0 check at 3,1 and 3, 8
+
+def rook_move(board, ori_x, ori_y):
+    for dx, dy in rook_direction:
+        i, j = ori_x + dx, ori_y + dy
+        while 0 <= i < len(board) and 0 <= j < len(board[i]):
+            if board[i][j] == '.':
+                board[i][j] = 'C'
+            elif board[i][j] == 'K':
+                return True
+                # print("Success")
+                # exit()
+            elif board[i][j] == 'C':
+                i += dx
+                j += dy
+            else:
+                break
+
+def bishop_move(board, ori_x, ori_y):
+    for dx, dy in bishop_direction:
+        i, j = ori_x + dx, ori_y + dy
+        while 0 <= i < len(board) and 0 <= j < len(board[i]):
+            if board[i][j] == '.':
+                board[i][j] = 'C'
+            elif board[i][j] == 'K':
+                return True
+                # print("Success")
+                # exit()
+            elif board[i][j] == 'C':
+                i += dx
+                j += dy
+            else:
+                break
+
+def queen_move(board, ori_x, ori_y):
+    for dx, dy in queen_direction:
+        i, j = ori_x + dx, ori_y + dy
+        while 0 <= i < len(board) and 0 <= j < len(board[i]):
+            if board[i][j] == '.':
+                board[i][j] = 'C'
+            elif board[i][j] == 'K':
+                return True
+                # print("Success")
+                # exit()
+            elif board[i][j] == 'C':
+                i += dx
+                j += dy
+            else:
+                break
+       
+def king_move(board, ori_x, ori_y):
+    check_count = 0
+    piece_count = 0
+    for dx, dy in king_direction:
+        i, j = ori_x + dx, ori_y + dy
+        if 0 <= i < len(board) and 0 <= j < len(board[i]):
+            if board[i][j] == 'C':
+                check_count += 1
+            elif board[i][j] == '.':
+                continue
+            elif board[i][j] == 'P' or 'R' or 'B' or 'Q':
+                piece_count += 1
+
+
+    return(check_count, piece_count)
+
+
+def start_board(board):
+    king_count = 0
+    pawn_count, rook_count, bishop_count, queen_count = 0, 0, 0, 0
+    king_in_check = False
+    for row in range(len(board)):
+        for space in range(len(board[row])):
+            if board[row][space] == 'K':
+                king_count += 1
+                if king_count != 1:
+                    print("Invalid Board: No King Found")
+                    exit()
+                # continue
+            elif board[row][space] == 'R':
+                rook_count += 1
+                if rook_move(board, row, space):
+                    king_in_check = True
+            elif board[row][space] == 'B':
+                bishop_count += 1
+                if bishop_move(board, row, space):
+                    king_in_check = True
+            elif board[row][space] == 'Q':
+                queen_count += 1
+                if queen_move(board, row, space):
+                    king_in_check = True
+            elif board[row][space] == 'P':
+                pawn_count += 1
+                if pawn_move(board, row, space):
+                    king_in_check = True
+    print(f"Pawn Count: {pawn_count}\n"
+          f"Rook Count: {rook_count}\n"
+          f"Bishop Count: {bishop_count}\n"
+          f"Queen Count: {queen_count}"
+          )
+    if king_count == 0:
+        print("Invalid Board: No King Found")
+        exit()
+    if king_in_check:
+        print("Success: King is in check")
+    else:
+        print("King is safe")
+
+
+def around_king(board):
+    for row in range(len(board)):
+        for space in range(len(board[row])):
+            if board[row][space] == 'K':
+                check_count, piece_count = king_move(board, row, space)
+    print(f"Check around king: {check_count}\n"
+          f"Piece around king: {piece_count}"
+          )
+    
--- rush/main.py
+++ rush/main.py
@@ -1,10 +1,34 @@
 import checkmate
+import sys
 
-board = '''
-R...
-.K..
-....
-....
-'''
+example_board = '''\
+..P..
+.....
+..K..
+...R.
+.....\
+''' 
 
-print(checkmate.game_state_iterator(board.split(), checkmate.check_board_size(board)))
+def create_board(example_board):
+    board = example_board.strip().splitlines()
+    board = [list(row) for row in board]
+    return board
+
+if len(sys.argv) == 1:
+    board = create_board(example_board)
+    checkmate.start_board(board)
+    checkmate.board_checker(board)
+    checkmate.print_board(board)
+    checkmate.around_king(board)
+else:
+    for filename in sys.argv[1:]:
+        with open(filename, 'r') as f:
+            content = f.read()
+            start = content.find("\\")
+            stop = content.find("\\", start + 1)
+            between = content[start + 1:stop]
+        board = create_board(between)
+        checkmate.start_board(board)
+        checkmate.board_checker(board)
+        checkmate.print_board(board)
+        checkmate.around_king(board)
